26c1c7cf92ad1a8969301efa1bcb1513b9e7e240
Ou <orongxing@gmail.com>
2024-09-30 01:50:58 +0800

    fix: proxy api

--- server/routes/[id].ts
+++ server/routes/[id].ts
@@ -4,5 +4,5 @@ export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id")
   const { latest } = getQuery(event)
   if (latest !== undefined) return await sendProxy(event, `https://smzdk.top/api/${id}/new`)
-  return await sendProxy(event, `https://smzdk.top/api/${id}/new`)
+  return await sendProxy(event, `https://smzdk.top/api/${id}`)
 })
