ae81649d29868852829d63f22252b0af152ed4a0
Ou <orongxing@gmail.com>
2024-10-09 02:54:11 +0800

    fix: nitro auto imporat in vitest

--- test/common.test.ts
+++ test/common.test.ts
@@ -1,24 +1,5 @@
-import { describe, expect, it } from "vitest"
-import { tranformToUTC } from "#/utils/date"
+import { it } from "vitest"
 
-describe("transform Beijing time to UTC in different timezone", () => {
-  const a = "2024/10/3 12:26:16"
-  const b = 1727929576000
-  it("in UTC", () => {
-    Object.assign(process.env, { TZ: "UTC" })
-    const date = tranformToUTC(a)
-    expect(date).toBe(b)
-  })
-
-  it("in Beijing", () => {
-    Object.assign(process.env, { TZ: "Asia/Shanghai" })
-    const date = tranformToUTC(a)
-    expect(date).toBe(b)
-  })
-
-  it("in New York", () => {
-    Object.assign(process.env, { TZ: "America/New_York" })
-    const date = tranformToUTC(a)
-    expect(date).toBe(b)
-  })
+it("test", () => {
+  //
 })
---
+++ vitest.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vitest/config"
+import autoImport from "unplugin-auto-import/vite"
+import tsconfigPath from "vite-tsconfig-paths"
+import { resolveModuleExportNames } from "mlly"
+
+const h3Exports = await resolveModuleExportNames("h3", {
+  url: import.meta.url,
+})
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["server/**/*.test.ts", "shared/**/*.test.ts"],
+  },
+  plugins: [
+    tsconfigPath(),
+    // https://github.com/unjs/nitro/blob/v2/src/core/config/resolvers/imports.ts
+    autoImport({
+      imports: ["vitest", {
+        from: "h3",
+        imports: h3Exports.filter(n => !/^[A-Z]/.test(n) && n !== "use"),
+      }, {
+        from: "ofetch",
+        imports: ["$fetch", "ofetch"],
+      }],
+      dirs: ["server/utils"],
+    }),
+  ],
+})
