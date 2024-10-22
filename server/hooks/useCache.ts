182af0cf9aeeca604ae1051888057f940c759d31
Ou <orongxing@gmail.com>
2024-10-23 03:18:50 +0800

    chore: disable cache when development

--- server/hooks/useCache.ts
+++ server/hooks/useCache.ts
@@ -4,7 +4,9 @@ import { Cache } from "#/database/cache"
 export function useCache() {
   try {
     const db = useDatabase()
-    if (db && process.env.NODE_ENV === "production") return new Cache(db)
+    if (process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== "production") return
+    if (process.env.NODE_ENV && process.env.NODE_ENV !== "production") return
+    if (db) return new Cache(db)
   } catch (e) {
     logger.error("failed to init database ", e)
   }
