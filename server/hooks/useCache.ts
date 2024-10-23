811776930c4e3598c9dcebed1c27b5c2217030ae
Ou <orongxing@gmail.com>
2024-10-24 00:35:50 +0800

    fix: cloudflare cache

--- server/hooks/useCache.ts
+++ server/hooks/useCache.ts
@@ -4,7 +4,7 @@ import { Cache } from "#/database/cache"
 export function useCache() {
   try {
     const db = useDatabase()
-    if (process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== "production") return
+    if (process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== "main") return
     if (process.env.NODE_ENV && process.env.NODE_ENV !== "production") return
     if (db) return new Cache(db)
   } catch (e) {
