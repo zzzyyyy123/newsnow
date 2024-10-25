f6a7b201baecbd19a50452d38ae0ecfb6e7a534b
Ou <orongxing@gmail.com>
2024-10-25 13:40:19 +0800

    chore: add github badge

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -80,6 +80,24 @@ export function Menu() {
                 <span className="i-ph:github-logo-duotone inline-block" />
                 <span>Star on Github </span>
               </li>
+              <li className="flex gap-2 items-center">
+                <a
+                  href="https://github.com/ourongxing/newsnow"
+                >
+                  <img
+                    alt="GitHub forks badge"
+                    src="https://img.shields.io/github/stars/ourongxing/newsnow?logo=github"
+                  />
+                </a>
+                <a
+                  href="https://github.com/ourongxing/newsnow/fork"
+                >
+                  <img
+                    alt="GitHub forks badge"
+                    src="https://img.shields.io/github/forks/ourongxing/newsnow?logo=github"
+                  />
+                </a>
+              </li>
             </ol>
           </motion.div>
         </div>
