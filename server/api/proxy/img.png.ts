c5feeff89a707f83f2d201dd73753c969e567827
Ou <orongxing@gmail.com>
2024-11-01 20:27:09 +0800

    fix: image proxy

--- server/api/proxy/img.png.ts
+++ server/api/proxy/img.png.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
   const { url: img, type = "encodeURIComponent" } = getQuery(event)
   if (img) {
-    const url = type === "encodeURIComponent" ? encodeURIComponent(img as string) : decodeBase64URL(img as string)
+    const url = type === "encodeURIComponent" ? decodeURIComponent(img as string) : decodeBase64URL(img as string)
     return sendProxy(event, url, {
       headers: {
         "Access-Control-Allow-Origin": "*",
