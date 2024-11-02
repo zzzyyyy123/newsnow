8263dbff71fee5d15f420502dfec2f4c18f8fe80
Ou <orongxing@gmail.com>
2024-11-03 01:00:39 +0800

    fix(source): producthunt items selector

--- server/sources/producthunt.ts
+++ server/sources/producthunt.ts
@@ -5,7 +5,7 @@ export default defineSource(async () => {
   const baseURL = "https://www.producthunt.com/"
   const html: any = await myFetch(baseURL)
   const $ = cheerio.load(html)
-  const $main = $("[data-test^=post-item]")
+  const $main = $("[data-test=homepage-section-0] [data-test^=post-item]")
   const news: NewsItem[] = []
   $main.each((_, el) => {
     const a = $(el).find("a").first()
