b3ec041e47b5e199eca2fa3de29c2c24ccacc6c0
Ou <orongxing@gmail.com>
2025-01-19 16:27:27 +0800

    chore: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import { ourongxing, react } from "@ourongxing/eslint-config"
 export default ourongxing({
   type: "app",
   // 貌似不能 ./ 开头，
-  ignores: ["src/routeTree.gen.ts", "imports.app.d.ts", "public/"],
+  ignores: ["src/routeTree.gen.ts", "imports.app.d.ts", "public/", ".vscode"],
 }).append(react({
   files: ["src/**"],
 }))
