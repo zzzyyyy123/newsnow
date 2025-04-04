4d87df1009751e08000a73e6754f306f38ef2283
Ou <orongxing@gmail.com>
2025-04-04 19:59:44 +0800

    chore: minor changes

--- server/database/cache.ts
+++ server/database/cache.ts
@@ -74,7 +74,7 @@ export class Cache {
 export async function getCacheTable() {
   try {
     const db = useDatabase()
-    logger.info("db: ", db.getInstance())
+    // logger.info("db: ", db.getInstance())
     if (process.env.ENABLE_CACHE === "false") return
     const cacheTable = new Cache(db)
     if (process.env.INIT_TABLE !== "false") await cacheTable.init()
